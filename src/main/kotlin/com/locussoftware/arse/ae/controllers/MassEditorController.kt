944940a89ec622dad9925fd8ef1828b70ee05119
mikewlocus <mikewayne@locussoftware.com>
2022-05-30 10:22:53 +0100

    AE - Add message type limitation dropdown to mass editor

--- src/main/kotlin/com/locussoftware/arse/ae/controllers/MassEditorController.kt
+++ src/main/kotlin/com/locussoftware/arse/ae/controllers/MassEditorController.kt
@@ -1,13 +1,21 @@
 package com.locussoftware.arse.ae.controllers
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class MassEditorController {
 
+    /**
+     * Initialises the model objects for display on the mass editor UI
+     */
     @GetMapping("/mass-editor")
-    fun massEditor() : String {
+    fun massEditor(model: Model) : String {
+        // Initialise the type limitations
+        model["typeLimitations"] = listOf("COPARN", "COPRAR", "CUSCAR", "IFTMBF", "IFTMIN")
+
         return "/mass-editor"
     }
 
