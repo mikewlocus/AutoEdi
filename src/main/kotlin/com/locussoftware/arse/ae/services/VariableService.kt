c0782005b624b540f3b7c45a3b4022b109ebac1f
mikewlocus <mikewayne@locussoftware.com>
2022-06-01 10:43:35 +0100

    AE - Add variable loading from CSV
    Variables are now loaded into the database from the variables CSV, if the database table is empty.

--- src/main/kotlin/com/locussoftware/arse/ae/services/VariableService.kt
+++ src/main/kotlin/com/locussoftware/arse/ae/services/VariableService.kt
@@ -3,12 +3,63 @@ package com.locussoftware.arse.ae.services
 import com.locussoftware.arse.ae.entities.Variable
 import com.locussoftware.arse.ae.repositories.VariableRepository
 import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.File
 
 @Service
 class VariableService(val db: VariableRepository) {
 
+    /**
+     * Loads variables from the CSV if the database table is empty, then returns a list of variables from the database.
+     */
     fun getVariables() : List<Variable> {
+        // Load from CSV if empty
+        if(db.count() == 0L) {
+            this.loadVariablesFromCsv()
+        }
+
         return db.findVariables()
+    }
+
+    /**
+     * Reads through the variable CSV, imports each row into the variables table.
+     */
+    private fun loadVariablesFromCsv() {
+        // Load input from file
+        val bufferedCustomFieldReader: BufferedReader = File("variables.csv").bufferedReader()
+        val customFieldCsv = bufferedCustomFieldReader.use { it.readText() }
+
+        val fields = customFieldCsv
+            .replace("\"\"", "&dQuot1653061221")
+            .replace("\"", "")
+            .replace("&dQuot1653061221", "\"")
+            .split(System.lineSeparator())
+
+        // Loop through fields, save to db
+        fields.forEach {
+            val splitField = it.split(",")
+
+            val descIdx = 0
+            val nameIdx = 1
+            val codeIdx = 2
+            val typeIdx = 3
+            val checkIdx = 4
+            val paramReqIdx = 5
+            val paramInIdx = 6
+            val addCheckIdx = 8
+
+            if(splitField.size > addCheckIdx) {
+                db.save(Variable(null,
+                    splitField[descIdx],
+                    splitField[nameIdx],
+                    splitField[codeIdx],
+                    splitField[typeIdx],
+                    splitField[checkIdx],
+                    splitField[paramReqIdx],
+                    splitField[paramInIdx],
+                    splitField[addCheckIdx]))
+            }
+        }
     }
 
 }
