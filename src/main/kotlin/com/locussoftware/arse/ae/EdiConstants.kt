6763c2d2ff915038ad37b67cfb0441e09fcd45ee
mikewlocus <mikewayne@locussoftware.com>
2022-06-07 10:26:51 +0100

    AE - Add missing supported message type to the constants

--- src/main/kotlin/com/locussoftware/arse/ae/EdiConstants.kt
+++ src/main/kotlin/com/locussoftware/arse/ae/EdiConstants.kt
@@ -1,6 +1,6 @@
 package com.locussoftware.arse.ae
 
 object EdiConstants {
-    val messageTypes = listOf("APERAK", "BAPLIE", "CODECO", "COPARN", "COPRAR", "COREOR", "CUSCAR", "CUSRES", "DESTIM", "WESTIM", "IFCSUM", "IFTMBC", "IFTMBF", "IFTMCS", "IFTMIN", "IFTSAI", "IFTSTA", "VERMAS")
+    val messageTypes = listOf("APERAK", "BAPLIE", "CODECO", "COPARN", "COPRAR", "COREOR", "CUSCAR", "CUSRES", "DESTIM", "WESTIM", "IFCSUM", "IFTDGN", "IFTMBC", "IFTMBF", "IFTMCS", "IFTMIN", "IFTSAI", "IFTSTA", "VERMAS")
     val versions = listOf("D93A", "D93B", "D94A", "D94B", "D95A", "D95B", "D96A", "D96B", "D97A", "D97B", "D98A", "D98B", "D99A", "D99B", "D00A", "D00B", "D03A", "D03B", "D03A", "D03B", "D05A", "D05B", "D06A", "D06B", "D16A", "D16B")
 }
