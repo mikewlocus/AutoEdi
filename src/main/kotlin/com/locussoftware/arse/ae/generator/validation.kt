2ce0cb18c88f8bf5ffdef2b488223c167031c47b
mikewlocus <mikewayne@locussoftware.com>
2022-06-17 12:25:28 +0100

    AE - Add square bracket validation to satisfy all test conditions

--- src/main/kotlin/com/locussoftware/arse/ae/generator/validation.kt
+++ src/main/kotlin/com/locussoftware/arse/ae/generator/validation.kt
@@ -7,6 +7,26 @@ package com.locussoftware.arse.ae.generator
  * @return True if valid, false if not.
  */
 fun validateSquareBrackets(arsecode: String) : Boolean {
+    // Check the number of open and closing brackets match
+    if(arsecode.split("[").size != arsecode.split("]").size) {
+        return false
+    }
+
+    // Check all conditions have a result
+    arsecode.split("]").forEach {
+        // Ensure the space between the last ']' and the next '[' (if it exists) contains no whitespace
+        if(it.isEmpty() || (it.first() != '[' && it.trim().split("[")[0].isBlank())) {
+            return false
+        }
+    }
+
+    // Check for no further conditions after else
+    val splitOnElse = arsecode.split("[]")
+    if(splitOnElse.size > 1
+        && (splitOnElse[1].contains("[")
+                || splitOnElse[1].contains("]"))) {
+        return false
+    }
 
     return true
 }
